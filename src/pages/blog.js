a7df6f8ea06effcc02168ab54e12e8bfdd5d7e5c
Ryan Browne <brownetown7@gmail.com>
2019-06-17 19:13:03 +0100

    Adds new file structure

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <p>Post will show up here later</p>
+    </div>
+  )
+}
+
+export default BlogPage
