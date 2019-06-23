5d03cfb245aa48b2a89c500c47ba2ec239402962
Ryan Browne <brownetown7@gmail.com>
2019-06-23 14:00:09 +0100

    Fixes issue with FAQ wording

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -218,9 +218,7 @@ const LegalPage = () => {
             <h3>Cookies Policy </h3>
             <ul>
               <li>Our website uses cookies to distinguish you from other users of our website. This helps us to provide you with a good experience when you browse our website and also allows us to improve our site. By continuing to browse the site, you are agreeing to our use of cookies.</li>
-              <li>
-              •      A cookie is a small file of letters and numbers that we store on your browser or the hard drive of your computer if you agree. Cookies contain information that is transferred to your computer’s hard drive.
-                We use the following cookies:
+              <li>A cookie is a small file of letters and numbers that we store on your browser or the hard drive of your computer if you agree. Cookies contain information that is transferred to your computer’s hard drive.We use the following cookies:
               </li>
               <li>Strictly necessary cookies. These are cookies that are required for the operation of our website. They include, for example, cookies that enable you to log into secure areas of our website, use a shopping cart or make use of e-billing services.</li>
               <li>Analytical/performance cookies. They allow us to recognise and count the number of visitors and to see how visitors move around our website when they are using it. This helps us to improve the way our website works, for example, by ensuring that users are finding what they are looking for easily.</li>
