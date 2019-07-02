1e8b1864b7c8b21a49232da5739a7a07f2e19a0b
Ryan Browne <brownetown7@gmail.com>
2019-07-02 19:31:21 +0100

    Adds font loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        custom: {
+          families: ['Averta: n3,n4,n6,n7']
+        }
+      } 
+    }
   ]
 }
