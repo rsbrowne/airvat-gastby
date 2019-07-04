bd7f4eef39198738ff6efe34c33b5a520a976884
Ryan Browne <brownetown7@gmail.com>
2019-07-04 20:07:46 +0100

    Attempted addition of page loader

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
+import { css } from '@emotion/core';
+import { BeatLoader } from 'react-spinners';
 
 import Header from '../components/header'
 import Footer from '../components/footer'
 
 import '../styles/main.scss'
 
+const override = css`
+    display: block;
+    margin: 0 auto;
+`;
+
 const Layout = (props) => {
+
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    document.querySelector('.pre-loader').classList.add('active');
+    document.querySelector('.pre-loader').classList.remove('hidden');
+
+    setTimeout(() => {
+      document.querySelector('.pre-loader').classList.remove('active');
+      setLoad(false);
+    }, 1500);
+
+    setTimeout(() => {
+      document.querySelector('.pre-loader').classList.add('hidden');
+    }, 2000);
+  });
+
   return (
     <React.Fragment>
       <Helmet>
@@ -19,6 +43,15 @@ const Layout = (props) => {
         <link rel="canonical" href="http://www.airvat.co.uk" />
         <meta name="robots" content="noindex, nofollow" />
       </Helmet>
+      <div className="pre-loader active">
+        <BeatLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={'#123abc'}
+            loading={load}
+          />
+      </div>
       <Header></Header>
       {props.children}
       <Footer></Footer>
