911c08b425259e7ed53c76b31c681eb151eed076
Ryan Browne <brownetown7@gmail.com>
2019-07-29 22:42:27 +0100

    Adds basic structure to test blog layout

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import { useStaticQuery, graphql } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -12,6 +12,9 @@ const BlogPage = () => {
             frontmatter {
               title
               date
+            }
+            fields {
+              slug
             }
           }
         }
@@ -26,8 +29,10 @@ const BlogPage = () => {
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
             <li>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+              </Link>
             </li>
           )
         })}
