6b40db00afbcb5bf463d64e9fb202d880909e3da
Ryan Browne <brownetown7@gmail.com>
2019-07-29 23:09:11 +0100

    Adds some basic styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -24,19 +24,25 @@ const BlogPage = () => {
 
   return (
     <Layout>
-      <h1>Blog Page</h1>
-      <ul>
-        {data.allMarkdownRemark.edges.map((edge) => {
-          return (
-            <li>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+      <div className="section section--banner section--blog-list">
+        <div className="section__inner section__inner--blog-list">
+          <h1>Blog</h1>
+          <div className="blog-list">
+            {data.allMarkdownRemark.edges.map((edge) => {
+              return (
+                <div className="blog-card">
+                  <div className="blog-card__inner">
+                    <Link to={`/blog/${edge.node.fields.slug}`}>
+                      <h2>{edge.node.frontmatter.title}</h2>
+                      <p>{edge.node.frontmatter.date}</p>
+                    </Link>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
