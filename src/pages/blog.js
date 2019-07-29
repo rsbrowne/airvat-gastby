3badd6e8a3b7b5dbee036b0bf2df0095cc4c0f3a
Ryan Browne <brownetown7@gmail.com>
2019-07-29 20:23:58 +0100

    Adds first query to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import { useStaticQuery, graphql } from 'gatsby';
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
   return (
     <Layout>
       <h1>Blog Page</h1>
