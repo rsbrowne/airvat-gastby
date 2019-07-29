e4e0f792d893df5006c3fadfa39ab25c116faec5
Ryan Browne <brownetown7@gmail.com>
2019-07-29 22:19:35 +0100

    Adds code for creating blog pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
 
@@ -10,5 +12,34 @@ module.exports.onCreateNode = ({ node, actions }) => {
       value: slug
     });
   }
+
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogTemplate = path.resolve('./src/templates/blog.js');
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  res.data.allMarkdownRemark.edges.forEach((edge) => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug
+      }
+    });
+  });
 
 }
