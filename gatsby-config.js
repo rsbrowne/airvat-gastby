a74e068bf55aee85c7098390a9fe4631842d9268
Ryan Browne <brownetown7@gmail.com>
2019-10-07 18:21:13 +0100

    Adds gatsby image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,15 @@ module.exports = {
         path: `${__dirname}/blog/`
       }
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     'gatsby-transformer-remark',
     'gatsby-plugin-favicon',
     'gatsby-plugin-sitemap',
